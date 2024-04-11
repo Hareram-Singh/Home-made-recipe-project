8d520722babb6b75bfe0a3f4e85b466e5a07c0eb
Hareram-Singh <148152839+Hareram-Singh@users.noreply.github.com>
2024-04-11 21:22:21 +0530

    Update connection.js

--- backend/connection.js
+++ backend/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const url = ('mongodb+srv://hareramsingh5510:Hare2181@cluster0.reymmhh.mongodb.net/Ankush?retryWrites=true&w=majority&appName=Cluster0')
+const url = ('mongodb+srv://userid: Password@cluster0.reymmhh.mongodb.net/Ankush?retryWrites=true&w=majority&appName=Cluster0')
 mongoose.connect(url)
 .then((result) => {
     console.log('database connected')
